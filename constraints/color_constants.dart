1a36408bdee0e6ef42f6077dc8f009be2573a3c2
dongmin7208 <dongmin.park.career@gmail.com>
2023-07-05 23:56:15 +0900

    ADD:App_Color

---
+++ constraints/color_constants.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class AppConst {
+  AppConst._();
+  static const Color kMainTitle = Color(0xFF09051C);
+  static const Color kMainGreen = Color(0xFF53E88B);
+  static const Color kMainWhite = Color(0xFFFFFFFF);
+  static const Color kMainInput = Color(0xFFDA6317);
+  static const Color kMainInputBg = Color(0xFFF9A84D);
+  static const Color kMainLink = Color(0xFFFF7C32);
+}
