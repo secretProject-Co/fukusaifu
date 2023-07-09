3cbda92e780b01b9bf8b5f220d020ff2865a564b
dongmin7208 <dongmin.park.career@gmail.com>
2023-07-09 18:56:17 +0900

    ADD:Onboarding-2 onboarding_screen

--- lib/onboarding/screens/onboarding_screen.dart
+++ lib/onboarding/screens/onboarding_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fukusaifu/constants/color_constants.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({Key? key}) : super(key: key);
@@ -7,9 +8,34 @@ class OnboardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Onboarding Screen"),
-        titleTextStyle: TextStyle(color: AppConst.kMainTitle),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/images/logo/logo-fukusaifu.png",
+              width: 100,
+              height: 100,
+            ),
+            Text(
+              "副業をお探しですか？",
+              style: GoogleFonts.notoSans(
+                color: AppConst.kMainGreen,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "では、Fukusaifuへようこそ",
+              style: GoogleFonts.notoSans(
+                color: AppConst.kMainTitle,
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
