3ea595bf0323d8b84255c636e51086f0d1f296b8
mincheol <homerjay@naver.com>
2023-07-14 23:12:46 +0900

    ADD: BottomBar

---
+++ lib/home/screens/chat_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+          "Chat Screen!!!!"
+      ),
+    );
+  }
+}
