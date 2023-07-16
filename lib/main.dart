5e79150cf687a276ec7b173000625f52a97ae842
dongmin7208 <dongmin.park.career@gmail.com>
2023-07-16 19:05:30 +0900

    ADD:flutter_screenutil

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fukusaifu/auth/screens/sign_up_success_screen.dart';
 import 'package:fukusaifu/home/screens/home.dart';
 import 'package:fukusaifu/splash/splash.dart';
@@ -18,24 +19,27 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        '/': (context) => const Splash(),
-        '/onboardingScreen': (context) => const OnboardingScreen(),
-        '/onboardingScreen2': (context) => const OnboardingScreen2(),
-        '/signinScreen': (context) => const SignInScreen(),
-        '/signupScreen': (context) => const SignUpScreen(),
-        '/signupScreenProcess': (context) => const SignUpScreenProcess(),
-        '/signupUploadPhoto': (context) => const SignUpUploadPhoto(),
-        '/signupSuccessScreen': (context) => const SignupSuccessScreen(),
-        '/signupUploadPreView': (context) => const SignupUploadPreView(),
-        '/home': (context) => const Home(),
-      },
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      builder: (context, child) => MaterialApp(
+        routes: {
+          '/': (context) => const Splash(),
+          '/onboardingScreen': (context) => const OnboardingScreen(),
+          '/onboardingScreen2': (context) => const OnboardingScreen2(),
+          '/signinScreen': (context) => const SignInScreen(),
+          '/signupScreen': (context) => const SignUpScreen(),
+          '/signupScreenProcess': (context) => const SignUpScreenProcess(),
+          '/signupUploadPhoto': (context) => const SignUpUploadPhoto(),
+          '/signupSuccessScreen': (context) => const SignupSuccessScreen(),
+          '/signupUploadPreView': (context) => const SignupUploadPreView(),
+          '/home': (context) => const Home(),
+        },
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
       ),
     );
   }
